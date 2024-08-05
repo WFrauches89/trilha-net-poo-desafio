7bca8a372ec59e723a7c354e05a606b0bed87e2f
Wanderson Frauches <wfrauches89@gmail.com>
2024-08-05 15:54:59 -0300

    Realizando desafio POO - DOTNET

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia newNokia = new Nokia("22999999999", "Ultra", "Mobile - xc897", 64);
+
+newNokia.Ligar();
+newNokia.ReceberLigacao();
+newNokia.InstalarAplicativo("Snake Game");
+
+Iphone newIphone = new Iphone("2211111111", "16 Pro Max", "Mobile - Iphone csrs4", 32);
+
+newIphone.Ligar();
+newIphone.ReceberLigacao();
+newIphone.InstalarAplicativo("Snake Game");
+
